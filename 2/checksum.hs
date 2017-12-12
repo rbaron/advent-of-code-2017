3960e8e796ef2b4ef4822743323710a64e79835f
rbaron <rbaron@rbaron.net>
2017-12-12 18:44:14 +0100

    Oops, forgot part #2 of day 2.

--- 2/checksum.hs
+++ 2/checksum.hs
@@ -10,10 +10,16 @@ checksumLine line =
       min  = minimum ns
   in max - min
 
-checksum :: [String] -> Int
-checksum ls = sum $ map checksumLine ls
+checksumLine2 :: String -> Int
+checksumLine2 line =
+  let ws     = words line
+      ns     = map read ws :: [Int]
+      combs  = [ (i, j) | i <- ns, j <- ns, i /= j && i `rem` j == 0]
+      (i, j) = head combs
+      in i `div` j
 
 main = do
   content  <- readFile "input.txt"
   let ls = lines content
-      in putStrLn $ show $ checksum ls
+      in do print $ sum $ map checksumLine ls
+            print $ sum $ map checksumLine2 ls
