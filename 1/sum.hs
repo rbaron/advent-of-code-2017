576c2657c9f098d397325ff2041b35991823b55a
rbaron <rbaron@rbaron.net>
2017-12-12 18:34:06 +0100

    Oops, forgot part #2 of day 1.

--- 1/sum.hs
+++ 1/sum.hs
@@ -2,15 +2,22 @@ import Data.Char
 import System.IO
 
 
-getSum :: Char -> [Char] -> Int
+getSum :: Int -> [Int] -> Int
 getSum _ [] = 0
 getSum lastChar (c:cs) = if lastChar == c
-  then digitToInt lastChar + getSum c cs
+  -- then digitToInt lastChar + getSum c cs
+  then lastChar + getSum c cs
   else getSum c cs
+
+getSum2 :: [Int] -> Int
+getSum2 lst =
+  let shifted = drop (length lst `div` 2) $ cycle lst
+      in sum $ map (\(x, y) -> if x == y then x else 0) $ zip lst shifted
 
 main = do
   handle   <- openFile "input.txt" ReadMode
   contents <- hGetContents handle
-  let stripped = filter (/= '\n') contents in
+  let stripped = map digitToInt $ filter (/= '\n') contents in do
     putStrLn $ show $ getSum (last $ stripped) stripped
+    print $ getSum2 stripped
   hClose handle
